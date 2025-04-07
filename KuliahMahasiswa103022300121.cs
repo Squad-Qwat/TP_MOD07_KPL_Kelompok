b2c2fc7f6c68cce9eed43f30ed88da701b47dc33
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-04-08 02:54:53 +0700

    menambahkan class DataMahasiswa103022300121

---
+++ KuliahMahasiswa103022300121.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static tpmod07_kelompok.DataMahasiswa103022300121;
+using static tpmod07_kelompok.KuliahMahasiswa103022300121;
+
+namespace tpmod07_kelompok
+{
+    class KuliahMahasiswa103022300121
+    {
+      
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmod07_kelompok;
+
+DataMahasiswa103022300121 dataMahasiswa103022300121 = new DataMahasiswa103022300121();
+KuliahMahasiswa103022300121 kuliahMahasiswa103022300121 = new KuliahMahasiswa103022300121();
+
+// file json terletak pada nama_project\bin\Debug\net8.0
+dataMahasiswa103022300121.ReadJSON("tp7_1_103022300121.json");
